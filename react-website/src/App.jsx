7669ec6d883f73f36a28147540d37cfd20cda60c
Priyansh Khatri <priyanshkhatri8690@gmail.com>
2025-12-10 14:24:36 +0530

    practiced api fetching , for now it is only displaying the name of the pokemon , in next step will we fetching the image also

--- react-website/src/App.jsx
+++ react-website/src/App.jsx
@@ -5,50 +5,35 @@ export default function App(){
   const [query,setQuery]=useState("");
   const[data,setData]=useState([]);
   
-  
+  async function fetchingApi(){
+    const fetched= await fetch("https://pokeapi.co/api/v2/pokemon?limit=1500");
+    const response=await fetched.json();
+    const cleaner= response.results.map((item)=>({
+      naam:item.name,
+
+    }))
+    setData(cleaner)};
+    
+ 
   
   useEffect(() => {
-  async function loadData() {
-    try {
-      // STEP 1: Fetch the list of Pokemon (first 151)
-      const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
-      const list = await res.json();
-
-      // STEP 2: Fetch details for each Pokémon
-      const detailedData = await Promise.all(
-        list.results.map(async (pokemon) => {
-          const detailRes = await fetch(pokemon.url);
-          const detailData = await detailRes.json();
-
-          return {
-            id: detailData.id,
-            name: detailData.name,
-            img: detailData.sprites.front_default, // image URL
-          };
-        })
-      );
-
-      setData(detailedData);
-    } catch (err) {
-      console.error("Error loading Pokémon", err);
-      setData([]);
-    }
-  }
-
-  loadData();
-}, []);
+    fetchingApi();}
+     , []);
 
 
 
 
 
-
-
-
-        const filteredData =
+  const filteredData =
   query.trim() === ""
     ? data
-    : data.filter((item)=>item.name.toLowerCase().includes(query.toLowerCase()))
+    : data.filter((item) =>
+        item.naam.toLowerCase().includes(query.toLowerCase())
+      );
+
+
+
+        
 
 
 
@@ -67,14 +52,13 @@ export default function App(){
     <>
     <div className="text-2xl">
     <SearchBar query={query} setQuery={setQuery}/>
-    </div>
+    </div> 
     
     <div className="grid grid-cols-3 gap-4 mt-4 bg-yellow-100">
   {filteredData.map((item) => (
     <Card 
-      key={item.id}
-      img={item.img}
-      title={item.name}
+      key={item.naam}
+      title={item.naam}
     />
   ))}
 </div>
--- react-website/src/components/Card.jsx
+++ react-website/src/components/Card.jsx
@@ -1,7 +1,7 @@
-export default function Card({img, title}){
+export default function Card({title}){
     return(
         <div className="border-2 mt-2  w-fit ">
-            <img src={img}/>
+           
             <h2>{title}</h2>
             
             
